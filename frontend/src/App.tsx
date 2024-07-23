8cc7e09fd22fa6e5a2a6c5f147009f721662be64
tscurry <scurrytimmy@gmail.com>
2024-07-23 19:09:37 -0400

    tested backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,4 @@
-import * as React from 'react';
-import { loginUser } from './api/auth';
-
 const App = () => {
-  React.useEffect(() => {
-    const x = async () => {
-      await loginUser({ username: 'poison crapo', password: 'NahNigga1!' });
-    };
-    void x();
-  }, []);
   return (
     <>
       <p></p>
