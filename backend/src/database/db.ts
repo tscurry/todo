2a99e700f21c867ac6cb1e0bd4a0f0bcfe486357
tscurry <scurrytimmy@gmail.com>
2025-04-15 05:20:16 -0400

    updated backend logic

--- backend/src/database/db.ts
+++ backend/src/database/db.ts
@@ -7,7 +7,7 @@ dotenv.config();
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: process.env.NODE_ENV === 'production',
+    rejectUnauthorized: false,
   },
   max: 20,
   min: 5,
