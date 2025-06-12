2dcc74c6dcfc134d96aec5146cf2a076b31c3378
tscurry <scurrytimmy@gmail.com>
2025-06-12 01:06:01 -0400

    fix: temp fix to token middleware

--- backend/src/middleware/token.middleware.ts
+++ backend/src/middleware/token.middleware.ts
@@ -1,21 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import jwt, { JwtPayload, VerifyErrors } from 'jsonwebtoken';
-import { User } from '../utils/types';
-
-interface TokenUser {
-  user_uid: string;
-  username: string;
-}
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function isTokenUser(payload: any): payload is TokenUser {
-  return (
-    typeof payload === 'object' &&
-    payload !== null &&
-    typeof payload.user_uid === 'string' &&
-    typeof payload.username === 'string'
-  );
-}
+// import { User } from '../utils/types';
 
 //for authenticated and unauthenticated users
 export const optionalAuthenticateToken = (req: Request, res: Response, next: NextFunction) => {
@@ -27,11 +12,11 @@ export const optionalAuthenticateToken = (req: Request, res: Response, next: Nex
     return next();
   }
 
-  jwt.verify(token, process.env.JWT_SECRET as string, (err, payload) => {
-    if (err || !isTokenUser(payload)) {
+  jwt.verify(token, process.env.JWT_SECRET as string, (err, user) => {
+    if (err) {
       req.user = undefined;
     } else {
-      req.user = payload as User;
+      req.user = user;
     }
     next();
   });
@@ -43,11 +28,9 @@ export const authenticateToken = (req: Request, res: Response, next: NextFunctio
 
   if (!token) return res.status(401).json({ error: 'Access token required' });
 
-  jwt.verify(token, process.env.JWT_SECRET as string, (err, payload) => {
-    if (err || !isTokenUser(payload)) {
-      return res.status(403).json({ error: 'Invalid or expired token. Access Denied.' });
-    }
-    req.user = payload as User;
+  jwt.verify(token, process.env.JWT_SECRET as string, (err, user) => {
+    if (err) return res.status(403).json({ error: 'Invalid or expired token. Access Denied.' });
+    req.user = user;
     next();
   });
 };
@@ -60,12 +43,80 @@ export const authenticateRefreshToken = (req: Request, res: Response, next: Next
   jwt.verify(
     refreshToken,
     process.env.JWT_REFRESH_SECRET as string,
-    (err: VerifyErrors | null, payload: string | JwtPayload | undefined) => {
-      if (err || !isTokenUser(payload)) {
+    (err: VerifyErrors | null, user: string | JwtPayload | undefined) => {
+      if (err)
         return res.status(403).json({ error: 'Invalid or expired refresh token. Access Denied.' });
-      }
-      req.user = payload as User;
+
+      req.user = user;
       next();
     },
   );
 };
+
+// interface TokenUser {
+//   user_uid: string;
+//   username: string;
+// }
+
+// // eslint-disable-next-line @typescript-eslint/no-explicit-any
+// function isTokenUser(payload: any): payload is TokenUser {
+//   return (
+//     typeof payload === 'object' &&
+//     payload !== null &&
+//     typeof payload.user_uid === 'string' &&
+//     typeof payload.username === 'string'
+//   );
+// }
+
+// //for authenticated and unauthenticated users
+// export const optionalAuthenticateToken = (req: Request, res: Response, next: NextFunction) => {
+//   const authHeader = req.headers['authorization'];
+//   const token = authHeader && authHeader.split(' ')[1];
+
+//   if (!token) {
+//     req.user = undefined;
+//     return next();
+//   }
+
+//   jwt.verify(token, process.env.JWT_SECRET as string, (err, payload) => {
+//     if (err || !isTokenUser(payload)) {
+//       req.user = undefined;
+//     } else {
+//       req.user = payload as User;
+//     }
+//     next();
+//   });
+// };
+
+// export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+//   const authHeader = req.headers['authorization'];
+//   const token = authHeader && authHeader.split(' ')[1];
+
+//   if (!token) return res.status(401).json({ error: 'Access token required' });
+
+//   jwt.verify(token, process.env.JWT_SECRET as string, (err, payload) => {
+//     if (err || !isTokenUser(payload)) {
+//       return res.status(403).json({ error: 'Invalid or expired token. Access Denied.' });
+//     }
+//     req.user = payload as User;
+//     next();
+//   });
+// };
+
+// export const authenticateRefreshToken = (req: Request, res: Response, next: NextFunction) => {
+//   const refreshToken = req.cookies?.refreshToken;
+
+//   if (!refreshToken) return res.status(404).json({ error: 'Refresh token required' });
+
+//   jwt.verify(
+//     refreshToken,
+//     process.env.JWT_REFRESH_SECRET as string,
+//     (err: VerifyErrors | null, payload: string | JwtPayload | undefined) => {
+//       if (err || !isTokenUser(payload)) {
+//         return res.status(403).json({ error: 'Invalid or expired refresh token. Access Denied.' });
+//       }
+//       req.user = payload as User;
+//       next();
+//     },
+//   );
+// };
